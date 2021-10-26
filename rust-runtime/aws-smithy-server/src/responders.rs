44ce16487215f4c7fd9c1c4f8fbda9b6c56f996b
david-perez <d@vidp.dev>
2021-10-26 13:43:06 +0200

    Cleanup in separate modules

---
+++ rust-runtime/aws-smithy-server/src/responders.rs
@@ -0,0 +1,38 @@
+use axum::response::IntoResponse;
+use http::Response;
+use hyper::Body;
+
+use crate::model::{HealthcheckOutput, RegisterServiceError, RegisterServiceOutput};
+
+// Operation output types.
+
+impl IntoResponse for HealthcheckOutput {
+    type Body = axum::body::Body;
+    type BodyError = <Self::Body as axum::body::HttpBody>::Error;
+
+    fn into_response(self) -> Response<Self::Body> {
+        Response::builder()
+            .body(Body::from(String::from("output::HealthcheckOutput has no fields, but we would read them here")))
+            .unwrap()
+    }
+}
+
+impl IntoResponse for RegisterServiceOutput {
+    type Body = axum::body::Body;
+    type BodyError = <Self::Body as axum::body::HttpBody>::Error;
+
+    fn into_response(self) -> Response<Self::Body> {
+        Response::builder().body(Body::from(String::from("RegisterServiceOutput TODO"))).unwrap()
+    }
+}
+
+// Operation error types.
+
+impl IntoResponse for RegisterServiceError {
+    type Body = axum::body::Body;
+    type BodyError = <Self::Body as axum::body::HttpBody>::Error;
+
+    fn into_response(self) -> Response<Self::Body> {
+        unimplemented!()
+    }
+}
---
+++ rust-runtime/aws-smithy-server/src/runtime.rs
@@ -0,0 +1,32 @@
+use axum::extract::RequestParts;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct AwsRestJson1<T>(pub T);
+
+pub(crate) fn json_content_type<B>(req: &RequestParts<B>) -> Result<bool, http::StatusCode> {
+    // TODO Replace this with a meaningful error.
+    let rejection = http::StatusCode::from_u16(400).unwrap();
+
+    let content_type = if let Some(content_type) = req.headers().ok_or(rejection)?.get(http::header::CONTENT_TYPE) {
+        content_type
+    } else {
+        return Ok(false);
+    };
+
+    let content_type = if let Ok(content_type) = content_type.to_str() {
+        content_type
+    } else {
+        return Ok(false);
+    };
+
+    let mime = if let Ok(mime) = content_type.parse::<mime::Mime>() {
+        mime
+    } else {
+        return Ok(false);
+    };
+
+    let is_json_content_type = mime.type_() == "application"
+        && (mime.subtype() == "json" || mime.suffix().filter(|name| *name == "json").is_some());
+
+    Ok(is_json_content_type)
+}
